a5ea4362399f1434a87f3ac36d2b81f43b8ca055
leonidbeltsov <leonidbeltsov@gmail.com>
2021-11-30 22:23:59 +0300

    fix ValidationCheck

--- src/test/java/ru/netology/web/HappyPathTest.java
+++ src/test/java/ru/netology/web/HappyPathTest.java
@@ -27,20 +27,30 @@ public class HappyPathTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");             // Запуск браузера без графического интерфейса
-//        options.addArguments("--start-maximized");      // Запуск в режиме максимального окна
-        options.addArguments("--incognito");            // Запуск браузера в режиме incognito
+// Запуск браузера без графического интерфейса
+        options.addArguments("--headless");
+// Запуск в режиме максимального окна
+//        options.addArguments("--start-maximized");
+// Запуск браузера в режиме incognito
+        options.addArguments("--incognito");
         driver = new ChromeDriver(options);
     }
 
     @Test
     public void shouldUseCssSelectors() {
+//        Открываем требуемый URL
         driver.get("http://localhost:9999");
+//        Находим поле ввода имени и фамилии и вводим в него строку
         driver.findElement(By.cssSelector("[data-test-id='name'] [name='name']")).sendKeys("Иванов Иван");
+//        Находим поле ввода номера телефона и вводим в него строку
         driver.findElement(By.cssSelector("[data-test-id='phone'] [name='phone']")).sendKeys("+79993332211");
+//        Находим CheckBox и кликаем по нему, ставя галочку
         driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+//        Находим кнопку "Продолжить" и кликаем по ней
         driver.findElement(By.cssSelector(".button__text")).click();
+//         ПОлучаем текст сообщения появляющегося при корректном заполнении всех полей и нажатии кнопки "Продолжить"
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+//        Сравниваем полученный текст с ожидаемым
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
--- src/test/java/ru/netology/web/ValidationCheck.java
+++ src/test/java/ru/netology/web/ValidationCheck.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidationCheck {
@@ -31,15 +35,63 @@ public class ValidationCheck {
     }
 
     @Test
-    public void shouldShowWarningMassageWithoutEverything() {
+//    public void test_000() {
+        public void shouldShowWarningMassageWithoutEverything() {
         driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String textUnderName = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", textUnderName.trim());
+    }
+
+
+    @Test
+//    public void test_000v2() {
+    public void shouldShowWarningMassageWithoutEverythingThroughWebElement() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        List<WebElement> elements = driver.findElements(By.cssSelector(".input_invalid .input__sub"));
+        String warningTextUnderName = elements.get(0).getText();
+        assertEquals("Поле обязательно для заполнения", warningTextUnderName.trim());
+//        String warningTextUnderPhone = elements.get(1).getText();
+//        assertEquals("Поле обязательно для заполнения", warningTextUnderPhone.trim());
+//        String checkboxWarningText = elements.get(2).getText();
+//        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", checkboxWarningText.trim());
+    }
+
+    @Test
+//    public void test_001() {
+        public void shouldShowWarningMassageWithCheckboxOnly() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String textUnderName = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", textUnderName.trim());
+    }
+
+    @Test
+//    public void test_010() {
+        public void shouldShowWarningMassageWithPhoneOnly() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] [name='phone']")).sendKeys("+79990003322");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String textUnderName = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", textUnderName.trim());
+    }
+
+    @Test
+//    public void test_011() {
+        public void shouldShowWarningMassageWithPhoneAndCheckBox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] [name='phone']")).sendKeys("+79993332211");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
         String textUnderName = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", textUnderName.trim());
     }
 
     @Test
-    public void shouldShowWarningMassageWithOnlyName() {
+//    public void test_100() {
+        public void shouldShowWarningMassageWithNameOnly() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] [name='name']")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector(".button__text")).click();
@@ -48,18 +100,8 @@ public class ValidationCheck {
     }
 
     @Test
-    public void shouldShowWarningMassageWithOnlyPhone() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] [name='phone']")).sendKeys("+79990003322");
-        driver.findElement(By.cssSelector(".button__text")).click();
-        String textUnderName = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub")).getText();
-        assertEquals("Поле обязательно для заполнения", textUnderName.trim());
-    }
-
-
-
-    @Test
-    public void shouldShowWarningMassageWithNameAndCheckbox() {
+//    public void test_101() {
+        public void shouldShowWarningMassageWithNameAndCheckbox() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] [name='name']")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id='agreement'] .checkbox__box")).click();
@@ -69,7 +111,18 @@ public class ValidationCheck {
     }
 
     @Test
-    public void shouldShowWarningMassageWithEnglishName() {
+//    public void test_110() {
+        public void shouldShowWarningMassageWithNameAndPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] [name='name']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] [name='phone']")).sendKeys("+79000000000");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String checkboxWarningText = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", checkboxWarningText.trim());
+    }
+
+    @Test
+    public void shouldShowWarningMassageWithEnglishNameOnly() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] [name='name']")).sendKeys("Sidorov Pavel");
         driver.findElement(By.cssSelector(".button__text")).click();
